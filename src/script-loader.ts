1b98d5897c1bb45a7580261995ad15d3fc9032a9
David Chin <davidchin.yf@gmail.com>
2019-09-20 09:28:21 +1000

    feat(common): CHECKOUT-4400 Load multiple scripts in parallel and run them in series

--- src/script-loader.ts
+++ src/script-loader.ts
@@ -1,5 +1,6 @@
 export default class ScriptLoader {
     private _scripts: { [key: string]: Promise<Event> } = {};
+    private _preloadedScripts: { [key: string]: Promise<Event> } = {};
 
     loadScript(src: string): Promise<Event> {
         if (!this._scripts[src]) {
@@ -20,6 +21,52 @@ export default class ScriptLoader {
         }
 
         return this._scripts[src];
+    }
+
+    loadScripts(urls: string[]): Promise<Event[]> {
+        const events: Event[] = [];
+        let promise: Promise<Event> | undefined;
+
+        return Promise.all(urls.map(url => this._preloadScript(url)))
+            .then(() => {
+                urls.forEach(url => {
+                    if (promise) {
+                        promise = promise.then(() => this.loadScript(url));
+                    } else {
+                        promise = this.loadScript(url);
+                    }
+
+                    promise.then(event => events.push(event));
+                });
+
+                return promise;
+            })
+            .then(() => events);
+    }
+
+    private _preloadScript(url: string): Promise<Event> {
+        if (!this._preloadedScripts[url]) {
+            this._preloadedScripts[url] = new Promise((resolve, reject) => {
+                const preloadedScript = document.createElement('link') as HTMLLinkElement;
+
+                preloadedScript.as = 'script';
+                preloadedScript.rel = 'preload';
+                preloadedScript.href = url;
+
+                preloadedScript.onload = event => {
+                    resolve(event);
+                };
+
+                preloadedScript.onerror = event => {
+                    delete this._preloadedScripts[url];
+                    reject(event);
+                };
+
+                document.head.appendChild(preloadedScript);
+            });
+        }
+
+        return this._preloadedScripts[url];
     }
 }
 
